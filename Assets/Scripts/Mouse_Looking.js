673c69638a3344acfe900fef118029be1e0e49b9
Weiwei Hu <wae3wae@yahoo.com>
2015-10-12 17:21:24 -0400

    Movement
    Added Movement

---
+++ Assets/Scripts/Mouse_Looking.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var x_rotation : float = 0.0;
+var y_rotation : float = 0.0;
+
+var x_sensitivty : float = 5.0;
+var y_sensitivty : float = 5.0;
+
+function Update () 
+{
+	x_rotation += Input.GetAxis("Mouse X") * x_sensitivty;
+	y_rotation -= Input.GetAxis("Mouse Y") * y_sensitivty;
+	
+	transform.localEulerAngles = Vector3 (y_rotation, x_rotation, 0);
+}
---
+++ Assets/Scripts/Movement_Script.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+private var ship_top_speed : float = 0.25;
+var ship_speed : float = 0.0;
+function Update() 
+{
+    var controller : CharacterController = GetComponent(CharacterController);
+	var forward = transform.TransformDirection(Vector3.forward);
+	controller.Move(this.gameObject.transform.forward * ship_speed);
+	//Speed Up
+	if (Input.GetKey("w"))
+	{
+		if (ship_speed < ship_top_speed)
+		{
+			ship_speed += 0.1;
+			if (ship_speed > ship_top_speed)
+			{
+				ship_speed = ship_top_speed;
+			}
+		}
+	}
+	//Slow Down
+	if (Input.GetKey("s"))
+	{
+		if (ship_speed > 0.0)
+		{
+			ship_speed -= 0.2;
+			if (ship_speed < 0.0)
+			{
+				ship_speed = 0;
+			}
+		}
+	}
+}
