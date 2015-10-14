3267d08e532c660db000fee4be81ced72a49098c
Weiwei Hu <wae3wae@yahoo.com>
2015-10-14 16:39:34 -0400

    Revert "Finished WASDEQ"
    This reverts commit 8ff37c207c652c3aa43434ff786fecd99da81b6c.

--- Assets/Scripts/Movement_Script.js
+++ Assets/Scripts/Movement_Script.js
@@ -3,36 +3,14 @@
 private var ship_top_speed : float = 0.25;
 var ship_speed : float = 0.0;
 var resistive_value: float = 0.01;
-var moving_forward = false;
-var rotating_y = false;
+var MovingForward = false;
 
-var x_rotation : float = 0.0;
-var y_rotation : float = 0.0;
-
-var x_sensitivity : float = 5.0;
-var y_sensitivity : float = 5.0;
-
-var max_y_rotation : float = 90;
 //Returning to forward direction when no buttons pressed
 function Resist(){
-    if(!moving_forward){
+    if(!MovingForward){
         ship_speed -= resistive_value;
         if(ship_speed < 0)
             ship_speed = 0;
-    }   
-    
-    if(!rotating_y && y_rotation != 0){
-    	if(y_rotation > 0){
-    		y_rotation -= y_sensitivity * 1.5;
-    		if(y_rotation < 0)
-    			y_rotation = 0;
-    	}
-    	
-    if(y_rotation < 0){
-    		y_rotation += y_sensitivity * 1.5;
-    		if(y_rotation > 0)
-    			y_rotation = 0;
-    	}
     }
 }
 
@@ -45,7 +23,7 @@ function Update()
 	//Speed Up
 	if (Input.GetKey("w"))
 	{
-	    moving_forward = true;
+	    MovingForward = true;
 		if (ship_speed < ship_top_speed)
 		{
 			ship_speed += 0.1;
@@ -56,30 +34,6 @@ function Update()
 		}
 	}
 	else{
-	    moving_forward = false;
+	    MovingForward = false;
 	}
-	
-	if(Input.GetKey("a")){
-		x_rotation -= x_sensitivity; 
-	}
-	if(Input.GetKey("d")){
-		x_rotation += x_sensitivity;
-	}
-	if(Input.GetKey("q")){
-		rotating_y = true;
-		y_rotation -= y_sensitivity;
-		if(y_rotation < (-1)*max_y_rotation)
-			y_rotation = (-1) * max_y_rotation;
-		
-	}
-	else if(Input.GetKey("e")){
-		rotating_y = true;
-		y_rotation += y_sensitivity;
-		if(y_rotation > max_y_rotation)
-			y_rotation = max_y_rotation;
-	}
-	else
-		rotating_y = false;
-	
-	transform.localEulerAngles = Vector3 (y_rotation, x_rotation, 0);
 }
