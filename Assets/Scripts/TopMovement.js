7679204e9ca19b57a0fae5946707799e221c4a4b
Redash4 <jon.que1011@gmail.com>
2015-10-15 10:38:55 -0400

    Fixed infinite loops
    No more issues, can move left, right, and fowards

--- Assets/Scripts/TopMovement.js
+++ Assets/Scripts/TopMovement.js
@@ -1,28 +1,11 @@
 ﻿#pragma strict
 
-private var ship_top_speed : float = 0.25;
 var ship_speed : float = 0.0;
-var resistive_value: float = 0.01;
 var moving_forward = false;
 var ship_speed_z : float = 0.0;
 
-
-
-
-//Returning to rest when no button is pressed in the foward direction
-function Resist(){
-    if(!moving_forward){
-        ship_speed -= resistive_value;
-        if(ship_speed < 0)
-            ship_speed = 0;
-    }   
-    
-
-    }
-
 function Update() 
 {
-    Resist();
     var controller : CharacterController = GetComponent(CharacterController);
     var forward = transform.TransformDirection(Vector3.forward);
     controller.Move(this.gameObject.transform.forward * ship_speed);
@@ -32,24 +15,21 @@ function Update()
     if (Input.GetKey("w"))
     {
         moving_forward = true;
-        if (ship_speed < ship_top_speed)
-        {
-            ship_speed += 0.1;
-            if (ship_speed > ship_top_speed)
-            {
-                ship_speed = ship_top_speed;
-            }
-        }
+            ship_speed = 0.1;
     }
     else{
         moving_forward = false;
+        ship_speed = 0.0;
     }
 	
-    while(Input.GetKey("a")){
+    if(Input.GetKey("a")){
         ship_speed_z = -0.1;
     }
-    while(Input.GetKey("d")){
+    if(Input.GetKey("d")){
         ship_speed_z = 0.1;
+    }
+    if(!Input.GetKey("a") && !Input.GetKey("d")){
+        ship_speed_z = 0.0;
     }
 
 }
