e92f61a8e101f36007d0c2d2c67e9b56d45fa077
Weiwei Hu <wae3wae@yahoo.com>
2015-10-16 10:34:40 -0400

    Added Start +Enhanced Pause

--- Assets/Scripts/Pause_Menu.js
+++ Assets/Scripts/Pause_Menu.js
@@ -20,19 +20,19 @@
  
  function OnGUI()
  {
-   windowRect = new Rect(20, 20, 120, 100);
+   windowRect = new Rect(20, 20, 120, 120);
    if(isPause)
         GUI.Window(0, windowRect, DoMyWindow, "Pause Menu");
  }
  
  function DoMyWindow(windowID){
- 	if(GUI.Button(new Rect(10, 20, 100, 20), "Back to Main"))
- 		print("Got a click");
+     if (GUI.Button(new Rect(10, 20, 100, 20), "Back to Main"))
+         Application.LoadLevel("Start");
  	
- 	if(GUI.Button(new Rect(10, 40, 100, 20), "Options"))
+ 	if(GUI.Button(new Rect(10, 50, 100, 20), "Options"))
  		print("onOptions");
  	
- 	if(GUI.Button(new Rect(10, 60, 100, 20), "Exit Game"))
+ 	if(GUI.Button(new Rect(10, 80, 100, 20), "Exit Game"))
  		Application.Quit();
  	
  }
