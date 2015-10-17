96878a8345bb6c919aaca1520e5f3f94507af8e8
Weiwei Hu <wae3wae@yahoo.com>
2015-10-17 13:09:44 -0400

    AddedHealth

---
+++ Assets/Scripts/HealthBar.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+var health = 100;
+function OnGUI () {
+    GUI.Label (Rect (1320, 29, 100, 30),  health.ToString());
+    GUI.contentColor = Color.blue;
+    //GUI.Rect(100, 100, 100, 100);
+}
+
