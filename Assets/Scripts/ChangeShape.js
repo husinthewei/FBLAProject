784d15a06d53de22c49a905e3a69bdf091dabc94
Redash4 <jon.que1011@gmail.com>
2015-10-17 18:13:39 -0400

    Fixed FitThrough Bug
    Everything works, shape change, portal looks better, and the ship fits
    through the hole as a rectangle and not a sphere. Had to adjust the
    radius of the character controller

--- Assets/Scripts/ChangeShape.js
+++ Assets/Scripts/ChangeShape.js
@@ -4,25 +4,29 @@ var sphere : GameObject;
 var cube : GameObject;
 sphere = GameObject.Find("Ship");
 cube = GameObject.Find("Cube");
+var controller : CharacterController;
+controller = GetComponent.<CharacterController>();
+
+
 //disable GameObjects other than sphere to start
 cube.SetActive(false);
-collider.cube.GetComponent<BoxCollider>(enabled);
+
  
 function Update(){
     if (Input.GetKeyDown(KeyCode.F)){
         if(sphere.activeSelf == true){
-            cube.transform.position = sphere.transform.position;
+            //cube.transform.position = sphere.transform.position;
             sphere.SetActive(false);
             cube.SetActive(true);
-            collider.sphere.GetComponent<SphereCollider>(!enabled);
-            collider.cube.GetComponent<BoxCollider>(enabled);
-        }else if(cube.activeSelf == true){{
-            sphere.transform.position = cube.transform.position;
+	        controller.radius = 0.0;
+
+
+        }else if(cube.activeSelf == true){
+            //sphere.transform.position = cube.transform.position;
             cube.SetActive(false);
             sphere.SetActive(true);
-            collider.cube.GetComponent<BoxCollider>(!enabled);
-            collider.sphere.GetComponent<SphereCollider>(enabled);
+            controller.radius = 0.5;
+
         }
         }
     }
-}
