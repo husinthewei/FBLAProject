1918f82ebd6f2f2a151beabc4203b355f9ef8076
Weiwei Hu <wae3wae@yahoo.com>
2015-10-27 11:01:30 -0400

    Dimension2

---
+++ Assets/Scripts/LoadDimension2.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadDimension2 : MonoBehaviour {
+
+    void OnTriggerEnter()
+    {
+        Application.LoadLevel("Dimension2");
+    }
+
+}
