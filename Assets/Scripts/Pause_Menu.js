3c815d4a3c085609bccdfa73d0a16ce9c1dc5726
Weiwei Hu <wae3wae@yahoo.com>
2015-10-27 10:41:15 -0400

    Pause

---
+++ Assets/Scripts/Pause_Menu.js
@@ -0,0 +1,38 @@
+﻿var isPause = false;
+//public var 
+ 
+ 
+ function Update () {
+  if( Input.GetKeyDown(KeyCode.Escape))
+    {
+       //createWindow();
+       isPause = !isPause;
+       if(isPause)
+          Time.timeScale = 0;
+       else
+          Time.timeScale = 1;
+    }
+ }
+ 
+ function createWindow(){
+ 	//int width = 
+ }
+ 
+ function OnGUI()
+ {
+   windowRect = new Rect(20, 20, 120, 120);
+   if(isPause)
+        GUI.Window(0, windowRect, DoMyWindow, "Pause Menu");
+ }
+ 
+ function DoMyWindow(windowID){
+     if (GUI.Button(new Rect(10, 20, 100, 20), "Back to Main"))
+         Application.LoadLevel("Start");
+ 	
+ 	if(GUI.Button(new Rect(10, 50, 100, 20), "Options"))
+ 		print("onOptions");
+ 	
+ 	if(GUI.Button(new Rect(10, 80, 100, 20), "Exit Game"))
+ 		Application.Quit();
+ 	
+ }
---
+++ Assets/Scripts/Start_Menu.js
@@ -0,0 +1,18 @@
+﻿
+function OnGUI()
+{
+    var windowRect = Rect(20, 20, 120, 120);
+    windowRect = GUI.Window (0, windowRect, DoMyWindow, "Toast Simulator");
+}
+
+
+function DoMyWindow(windowID)
+{
+    if (GUI.Button(Rect(10, 20, 100, 20), "Start Game"))
+        Application.LoadLevel("Locomotion");
+
+    if (GUI.Button(Rect(10, 50, 100, 20), "Controls"));
+        
+    if (GUI.Button (Rect(10,80,100,20), "Quit"))
+        Application.Quit();
+}
