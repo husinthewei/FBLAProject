30a72662aec237ea9e1c9e350d282ebe625e5438
Weiwei Hu <wae3wae@yahoo.com>
2015-11-02 11:05:11 -0500

    ShapeChecker Initial
    Still have issues with changing scenes and using shapechecker/triggers
    b/c OnTriggerEnter

---
+++ Assets/Scripts/Two_D_OneResetNotInvisible.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Two_D_OneResetNotInvisible : MonoBehaviour {
+    public CharacterController controller;
+    void OnTriggerEnter()
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller.radius != 0.0F)
+            Application.LoadLevel("2D1");
+    }
+}
+
