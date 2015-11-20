325678e9b25625c37713a30d6321c2c3de53061d
Weiwei Hu <wae3wae@yahoo.com>
2015-11-19 19:09:12 -0500

    AttemptingTextures

--- Assets/Scripts/MultGame.js
+++ Assets/Scripts/MultGame.js
@@ -1,29 +1,61 @@
-﻿    var canvas;    
-    void Start()
-    {
-        canvas = GetComponent<Canvas>();
-        canvas.enabled = false;
-    }
-    
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Pause();
-        }
-    }
-    
-    public void Pause()
-    {
-        canvas.enabled = !canvas.enabled;
-        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
-    }
-    
-    public void Quit()
-    {
-        #if UNITY_EDITOR 
-        EditorApplication.isPlaying = false;
-        #else 
-        Application.Quit();
-        #endif
-    }
+﻿#pragma strict
+var health = 100;
+var x = 1320;
+var CurrentQuestion = 0;
+
+var factors = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var products = [0,0,0,0,0,0,0,0,0,0];
+
+var answers = [0,0,0,0,0,0,0,0,0,0];
+
+var one : Texture;
+one = Resources.Load("Resources/1");
+
+function Start(){
+	CreateNumbers();
+	assignTextures();
+}
+function assignTextures(){
+	one = Resources.Load("Resources/1");
+	//var go = new GameObject.CreatePrimitive(PrimitiveType.Plane);
+	//var rend = go.GetComponent.<Renderer>();
+	//rend.material.mainTexture = Resources.Load("glass") as Texture;
+} 
+
+function OnGUI () {
+	var string = "";
+	for(var i = 0; i < answers.length; i++){
+		string += answers[i];
+	}
+	GUI.DrawTexture(Rect(10,10,20,30), one);
+    GUI.Label (Rect (1000, 29, 100, 30), string);
+    GUI.contentColor = Color.blue;
+    //GUI.Rect(100, 100, 100, 100);
+    //x-=1;
+}
+
+function Update(){
+	OnGUI();
+	if(Input.GetMouseButtonDown(0)){
+		OnMouseDown();
+	}
+	
+	for(var i = 0; i < answers.length; i++){
+		answers[i] = parseInt(Random.value * 10);
+	}
+}
+
+function OnMouseDown(){
+	print('wassup');
+}
+function CreateNumbers(){
+	for(var i = 0; i < factors.length; i++){
+		factors[i] = parseInt(Random.value * 10);
+	}
+	for(var j = 0; j < products.length; j++){
+		products[j] = factors[2*j] * factors[2*j+1];
+		answers[j] = parseInt(Random.value * 10);
+	}
+}
+
+
