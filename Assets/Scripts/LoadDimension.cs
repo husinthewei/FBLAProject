4a2f567a38b57d946b7633a136641ba15267ef8f
Redash4 <jon.que1011@gmail.com>
2015-11-28 17:47:29 -0500

    works

---
+++ Assets/Scripts/LoadDimension.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadDimension : MonoBehaviour {
+
+    void OnTriggerEnter()
+    {
+		if(Application.loadedLevelName == "2D1"){
+        Application.LoadLevel("Dimension2");
+			Time.timeScale = 2;
+		}
+		else if(Application.loadedLevelName == "2D1_to_L"){
+			Application.LoadLevel("Locomotion");
+		}
+		else if(Application.loadedLevelName == "Dimension2"){
+			Application.LoadLevel("2D1_to_L");
+			Time.timeScale = 1;
+		}
+    }
+
+}
