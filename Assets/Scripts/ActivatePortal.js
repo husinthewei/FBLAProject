cbe33582109f86dbbb3f7a5870e08e552bec1e3a
Redash4 <jon.que1011@gmail.com>
2015-11-28 20:18:23 -0500

    Activate Portal

---
+++ Assets/Scripts/ActivatePortal.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+public static var OneDone = false;
+public static var TwoDone = false;
+var Portal : GameObject;
+Portal = GameObject.Find("Portal");
+function Start () {
+Portal.SetActive(true);
+}
+
+function Update () {
+if(OneDone == true && TwoDone == true){
+Portal.SetActive(true);
+}
+}
