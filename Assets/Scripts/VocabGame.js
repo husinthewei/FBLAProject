bc98c606fd5fed48dcff3e63baf5c7bad4039581
Weiwei Hu <wae3wae@yahoo.com>
2015-11-29 14:34:43 -0500

    Added Words

---
+++ Assets/Scripts/VocabGame.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var words = [["Approbation","Piquant","Gamin","Abbey"],["Coaltion", "Abbess", "Lode", "Designation"], ["Elicit", "Ameliorate", "Lather", "Signal"], ["Hackneyed", "Feasible", "Luxurious", "Novel"], ["Innuendo", "Statement", "Defense", "Feature"], ["Jaded","Inhibited","Disguised", "Defeated"],["Umbrage", "Pain", "Massacre", "Joy"], ["Ameliorate","Attack", "Faze", "Intrude"], ["Bombastic", "Humble", "Angry", "Annoying"], ["Callow", "Professional", "Skilled", "Unintentional"],["Epitome", "Opposite", "Scarab", "Harm"], ["Ingratiate", "Scare", "Attack", "Meet"], ["Lassitude", "Vigor", "Excitement", "Ecstatic"] , ["Permeate", "Stay", "Ensure", "Macadamize"], ["Enjoin", "Dissuade", "Anger", "Mark"], ["Nominal", "Set", "Intrusive", "Doubted"], ["Tenuous", "Firm", "Helpful", "Unaffected"] , ["Wheedle", "Dissuade", "Anger", "Juxtapose"] , ["Aura", "Quality" , "Surrounding", "Fade"] , ["Gossamer", "Metal", "Sheet", "Acetate"], ["Querulous", "Calm", "Excited", "Unreasonable"] , ["Repudiate","Compliment", "Encourage", "Help"] , ["Reverberate", "Stay", "Swing", "Run"]];
+//corect word will always be the first index. Later randomized order in output.
+//var words = ["Approbation","Coalition","Elicit","Hackneyed","Innuendo","Jaded","Umbrage","Ameliorate","Bombastic","Callow","Epitome","Ingratiate","Lassitude","Permeate","Enjoin","Nominal","Tenuous","Wheedle","Aura","Gossamer","Querulous","Repudiate","Reverberate"];
+var definitions = ["approval; commendation","a combination or alliance, especially a temporary one between persons, factions, states, etc","to draw or bring out or forth; educe; evoke","made commonplace or trite; stale; banal","an indirect intimation about a person or thing, especially of a disparaging or a derogatory nature.","dulled or satiated by overindulgence","offense; annoyance; displeasure","to make or become better, more bearable, or more satisfactory; improve"," high-sounding; high-flown; inflated; pretentious","immature or inexperienced","a person or thing that is typical of or possesses to a high degree the features of a whole class","to establish (oneself or someone else) in the favor or good graces of someone, especially by deliberate effort (usually followed by with): He ingratiated himself with all the guests","weariness of body or mind from strain, oppressive climate, etc.; lack of energy; listlessness; languor","to pass into or through every part of","to prescribe (a course of action) with authority or emphasis","being such in name only; so-called; putative","thin or slender in form, as a thread","to endeavor to influence (a person) by smooth, flattering, or beguiling words or acts","a distinctive and pervasive quality or character; air; atmosphere","a fine, filmy cobweb seen on grass or bushes or floating in the air in calm weather, especially in autumn","full of complaints; complaining","to reject as having no authority or binding force","to reecho or resound"];
+
+function Start () {
+
+}
+
+function OnGUI(){
+}
+
+function Update () {
+
+}
+
+function Update(){
+	time += Time.deltaTime;
+}
