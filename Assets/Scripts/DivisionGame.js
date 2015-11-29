289573f6d43bba5202685133b34866de0004119f
Weiwei Hu <wae3wae@yahoo.com>
2015-11-28 22:42:54 -0500

    Done Divison
    EZ

--- Assets/Scripts/DivisionGame.js
+++ Assets/Scripts/DivisionGame.js
@@ -1,13 +1,11 @@
 ﻿#pragma strict
-var health = 100;
-var x = 1320;
 private var CurrentQuestion = 0;
 private var time : float;
 private var seconds: float;
 private var TimeRemaining : float;
 TimeRemaining = 59;
-//randomized factors and the products of them
-private var factors = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+//randomized divisors and the products of them
+private var divisors = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 private var products = [0,0,0,0,0,0,0,0,0,0];
 
 //array of all of the inputted answers. Initially constantly randomized for visual effect.
@@ -45,38 +43,37 @@ function Start(){
 
 function OnGUI () {
 
-	Debug.Log(Screen.width + " " + Screen.height);
+	//Debug.Log(Screen.width + " " + Screen.height);
 	//var rx : float = Screen.width / native_width;
     //var ry : float = Screen.height / native_height;
     //GUI.matrix = Matrix4x4.TRS (Vector3(0, 0, 0), Quaternion.identity, Vector3 (rx, ry, 1));
 	//exit button
      if (GUI.Button(new Rect(10, 20, 100, 25), "Exit Puzzle")) {
-     
          Application.LoadLevel("Locomotion");	
      }
      
     //Checking key inputs to create the inputted answers
 	var e : Event = Event.current;
     if (Event.current.type == EventType.KeyUp && CurrentQuestion < 10){
-    	if(Event.current.keyCode == KeyCode.Alpha0 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha0 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 0;
-    	if(Event.current.keyCode == KeyCode.Alpha1 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha1 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 1;
-    	if(Event.current.keyCode == KeyCode.Alpha2 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha2 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 2;
-    	if(Event.current.keyCode == KeyCode.Alpha3 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha3 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 3;    		
-    	if(Event.current.keyCode == KeyCode.Alpha4 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha4 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 4;
-    	if(Event.current.keyCode == KeyCode.Alpha5 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha5 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 5;
-    	if(Event.current.keyCode == KeyCode.Alpha6 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha6 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 6;
-    	if(Event.current.keyCode == KeyCode.Alpha7 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha7 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 7;    	
-    	if(Event.current.keyCode == KeyCode.Alpha8 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha8 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 8;    
-    	if(Event.current.keyCode == KeyCode.Alpha9 && (CurrentAnswer).Length <2)
+    	if(Event.current.keyCode == KeyCode.Alpha9 && (CurrentAnswer).Length <1)
     		CurrentAnswer = CurrentAnswer + 9;
     		
    		//Backspace for answer		
@@ -90,8 +87,6 @@ function OnGUI () {
     				answers[CurrentQuestion] = 0;
     			if(CurrentAnswer.Length == 1)
     				answers[CurrentQuestion] = parseInt(CurrentAnswer.Substring(0,1));
-    			if(CurrentAnswer.Length == 2)
-    				answers[CurrentQuestion] = 10 * parseInt(CurrentAnswer.Substring(0,1)) + parseInt(CurrentAnswer.Substring(1,1));
     				
     			CurrentQuestion+=1;   		    					    	
     			CurrentAnswer = "";
@@ -110,13 +105,14 @@ function OnGUI () {
 		}
 		else{
 			GUI.DrawTexture(Rect (600, 280, 200, 200),wrong);
-			Application.LoadLevel("MultiplicationGame");
+			//reset();
+			Application.LoadLevel("DivisionGame");
 		}
 	}
 	//Debug.Log(TimeRemaining);
 	if(TimeRemaining <= 0){
 		GUI.DrawTexture(Rect (600, 280, 200, 200),wrong);
-		Application.LoadLevel("MultiplicationGame");
+		Application.LoadLevel("DivisionGame");
 	}
 	
 	
@@ -126,28 +122,25 @@ function OnGUI () {
 	for(var i = 0; i < answers.length; i++){
 		if(("" + answers[i]).Length == 1)
 			GUI.DrawTexture(Rect (462 + 50*i, 185, 20, 30),nums[answers[i]]);
-		if(("" + answers[i]).Length == 2){
-			GUI.DrawTexture(Rect (456 + 50*i, 185, 20, 30),nums[parseInt((""+answers[i]).Substring(0,1))]);
-			GUI.DrawTexture(Rect (456 + 50*i + 16, 185, 20, 30),nums[parseInt((""+answers[i]).Substring(1,1))]);
-			}
 	}
 	
-	//drawing factors
-	if(CurrentQuestion < 10){
-		GUI.DrawTexture(new Rect(395, 298, 60, 90), nums[factors[2 * CurrentQuestion]], ScaleMode.ScaleToFit, true);
-		GUI.DrawTexture(new Rect(520, 298, 60, 90), nums[factors[2 * CurrentQuestion + 1]], ScaleMode.ScaleToFit, true);
+	//drawing divisors
+	if(CurrentQuestion < 10 && (""+divisors[2*CurrentQuestion]).length == 1){
+		GUI.DrawTexture(new Rect(395, 298, 60, 90), nums[divisors[2 * CurrentQuestion]], ScaleMode.ScaleToFit, true);
+		GUI.DrawTexture(new Rect(520, 298, 60, 90), nums[divisors[2 * CurrentQuestion + 1]], ScaleMode.ScaleToFit, true);
 	}
+	if(CurrentQuestion < 10 && (""+divisors[2*CurrentQuestion]).length == 2){
+		GUI.DrawTexture(new Rect(380, 298, 60, 90), nums[parseInt((""+divisors[2 * CurrentQuestion]).Substring(0,1))], ScaleMode.ScaleToFit, true);
+		GUI.DrawTexture(new Rect(410, 298, 60, 90), nums[parseInt((""+divisors[2 * CurrentQuestion]).Substring(1,1))], ScaleMode.ScaleToFit, true);
+		GUI.DrawTexture(new Rect(520, 298, 60, 90), nums[divisors[2 * CurrentQuestion + 1]], ScaleMode.ScaleToFit, true);
+	}	
 	
 	Debug.Log(CurrentAnswer);
 	if(CurrentAnswer.Length == 1){
 		GUI.DrawTexture(new Rect(640, 298, 60, 90), nums[parseInt(CurrentAnswer.Substring(0,1))], ScaleMode.ScaleToFit, true);
 		}
-	if(CurrentAnswer.Length == 2){
-		GUI.DrawTexture(new Rect(640, 298, 60, 90), nums[parseInt(CurrentAnswer.Substring(0,1))], ScaleMode.ScaleToFit, true);
-		GUI.DrawTexture(new Rect(680, 298, 60, 90), nums[parseInt(CurrentAnswer.Substring(1,1))], ScaleMode.ScaleToFit, true);
-		}
 		
-	//GUI.DrawTexture(Rect (495, 500, 60, 90),nums[factors[2 * CurrentQuestion+1]]);
+	//GUI.DrawTexture(Rect (495, 500, 60, 90),nums[divisors[2 * CurrentQuestion+1]]);
 	
 	//GUI.DrawTexture(Rect (360, 240, 20, 30),two);
 	//GUI.Button (Rect (1000, 29, 100, 30),(one),GUIStyle.none);
@@ -212,12 +205,20 @@ function Update(){
 
 
 function CreateNumbers(){
-	for(var i = 0; i < factors.length; i++){
-		factors[i] = parseInt(Random.value * 10);
+	for(var i = 0; i < divisors.length; i++){
+		if(i%2==0)
+			divisors[i] = parseInt(Random.value * 10);
+		else{
+			var tempor = parseInt(Random.value * 10);
+			while(tempor == 0)
+				tempor = parseInt(Random.value * 10);
+			divisors[i] = tempor;
+		}
 	}
 	for(var j = 0; j < products.length; j++){
-		products[j] = factors[2*j] * factors[2*j+1];
-		answers[j] = parseInt(Random.value * 10);
+		var temp = divisors[2*j] * divisors[2*j+1];
+		products[j] = divisors[2*j];
+		divisors[2*j] = temp;
 	}
 }
 
