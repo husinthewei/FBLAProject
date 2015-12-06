c53ab91a46a21b903e92435bacdb6723a9482973
Weiwei Hu <wae3wae@yahoo.com>
2015-12-05 20:07:40 -0500

    DoneConcept

--- Assets/Scripts/Music Manager.js
+++ Assets/Scripts/Music Manager.js
@@ -9,13 +9,17 @@ function Update () {
 }
  
 function Awake () { 
-var go = GameObject.Find("Game Music"); 
+//var go = GameObject.Find("Game Music"); 
 
-var aud = go.GetComponent<AudioSource>();
+//var aud = go.GetComponent<AudioSource>();
 //Finds the game object called Game Music, if it goes by a different name, change this.
 //go.GetComponent<AudioSource>().clip = NewMusic; 
 
 
 //Replaces the old audio with the new one set in the inspector. 
-go.audio.Play(); //Plays the audio. 
+//go.audio.Play(); //Plays the audio. 
+
+
+	var audio : AudioSource = GetComponent.<AudioSource>();
+	audio.Play();
 }
